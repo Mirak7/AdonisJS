c5da9d14a431689e94d3d7b3cb2125fd8ef5f97f
p2414115 <bencheikh.a.karim@gmail.com>
2025-03-30 00:54:06 +0100

    delete old version

--- my-adonis-app/app/Middleware/LogRequest.ts
+++
@@ -1,8 +0,0 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class LogRequest {
-  public async handle({request}: HttpContextContract, next: () => Promise<void>) {
-    console.log(`Intercepting request METHOD:${request.method()} ROUTE:${request.url()}`)
-    await next()
-  }
-}
